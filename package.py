e3fd7c08ffc858146faa7cb25c186434368d677c
David Poliakoff <david.poliakoff@gmail.com>
2016-06-30 14:00:25 -0700

    Made this depend on the package it's supposed to depend on, rather than itself

--- package.py
+++ package.py
@@ -6,7 +6,7 @@ class TrivialOuter(Package):
     homepage = "http://www.example.com"
 
     version('git', git='https://github.com/DavidPoliakoff/trivial_outer.git', branch='master')
-    depends_on_git("https://github.com/DavidPoliakoff/trivial_outer")
+    depends_on_git("https://github.com/DavidPoliakoff/trivial")
 
     def install(self, spec, prefix):
         cmake('.',*std_cmake_args)
